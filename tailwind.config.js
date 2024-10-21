60b58dc787fe59d18288242f1b1ba14251821078
venkeyp333 <venkateshpujari333@gmail.com>
2024-10-21 14:57:37 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class', // Enable dark mode
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Ensure Tailwind scans your src folder for classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
